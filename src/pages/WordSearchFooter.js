60954282c52bcd641f5b924375d7e75717518108
Joel Miller <joeldmiller@gmail.com>
2023-07-26 12:47:55 -0400

    Added a joke of the day

--- src/pages/WordSearchFooter.js
+++ src/pages/WordSearchFooter.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const WordSearchFooter = () =>{
+  const [joke, setJoke] = useState([]);
+  useEffect(() => {
+    axios.get('https://official-joke-api.appspot.com/jokes/programming/random')
+      .then(response => {
+        setJoke(response.data[0].setup + ' ' + response.data[0].punchline);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <footer>
-        This is the bottom of the page.
+      {joke}
     </footer>
   );
 }
 export default WordSearchFooter;
+
+  
