84edbd8dd3af1cebee24b3117f97a5c3f4c75630
Joel Miller <joeldmiller@gmail.com>
2023-07-27 12:46:55 -0400

    Fixed the mobile display

--- src/pages/WordSearchWordList.js
+++ src/pages/WordSearchWordList.js
@@ -5,12 +5,12 @@ const WordSearchList = () =>{
   let charsTotal = 0;
   const wordList = ['CODE', 'KENTUCKY', 'JAVASCRIPT', 'CSS', 'HTML', 'REACT', 'COMPONENT', 'DIV', 'ARRAY', 'LOOP', 'CONST', 'VARIABLE'];
   for (let i = 0; i < wordList.length; i++){
-    words.push(<p>{wordList[i]}</p>)
+    words.push(<div className='word'>{wordList[i]}</div>)
     charsTotal += wordList[i].length;
   }
  
   return (
-    <div>
+    <div className='word-search-list-wrapper'>
       <h2>Word List</h2>
       <div className='word-search-list'>
         {words}
